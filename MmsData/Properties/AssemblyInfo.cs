8f5966852cff4eacdb7d50c8b745a945a8fb2146
Safaet Hossain <tuhin_42@hotmail.com>
2017-09-15 00:23:23 +0600

    few changed

--- MmsData/Properties/AssemblyInfo.cs
+++ MmsData/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VmsData")]
+[assembly: AssemblyTitle("MmsData")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VmsData")]
+[assembly: AssemblyProduct("MmsData")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- MmsEntity/Properties/AssemblyInfo.cs
+++ MmsEntity/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VmsEntity")]
+[assembly: AssemblyTitle("MmsEntity")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VmsEntity")]
+[assembly: AssemblyProduct("MmsEntity")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
