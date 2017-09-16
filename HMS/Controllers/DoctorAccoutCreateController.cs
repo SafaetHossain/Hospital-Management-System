1a0f51940d19180f32ceef6e2bf6ae66a54ca836
Safaet Hossain <tuhin_42@hotmail.com>
2017-09-17 00:47:39 +0600

    controller added

---
+++ HMS/Controllers/DoctorAccoutCreateController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HMS.Controllers
+{
+    public class DoctorAccoutCreateController : Controller
+    {
+        // GET: DoctorAccoutCreate
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CreateAccount()
+        {
+            return View();
+        }
+    }
+}
